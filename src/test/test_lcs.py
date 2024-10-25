0326fdec85b179108cfa55dd12d6524a809caf55
Laurenz Luke Gonzales <lgonza32@uncc.edu>
2024-10-25 17:14:54 -0400

    added stuff

--- src/test/test_lcs.py
+++ src/test/test_lcs.py
@@ -1,4 +1,8 @@
-from main.lcs import *
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+
+from src.main.lcs import *
 
 """
 Testing get_lcs function in lcs.py
