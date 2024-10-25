49452a92d9258ff972b793d29ebb3f0fca5d94ff
Laurenz Luke Gonzales <lgonza32@uncc.edu>
2024-10-25 15:09:19 -0400

    init commit

---
+++ src/main/main.py
@@ -0,0 +1,8 @@
+from lcs import *
+
+def main():
+    print(f"Hello World")
+    
+# Run the main function
+if __name__ == "__main__":
+    main()
---
+++ src/test/test_lcs.py
@@ -0,0 +1,15 @@
+from src.main.lcs import *
+
+"""
+Testing get_lcs function in lcs.py
+TODO: test for proper lcs length
+"""
+def test_get_lcs():
+    return 0
+
+"""
+Testing lcs_string function in lcs.py
+TODO: test for proper lcs output
+"""
+def test_lcs_string():
+    return 0
