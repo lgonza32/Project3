d32e867440d9cd2c61784b865a796705e390b1b1
Laurenz Luke Gonzales <lgonza32@uncc.edu>
2024-10-26 15:20:13 -0400

    Implemented main for LCS alg

--- src/main/main.py
+++ src/main/main.py
@@ -1,7 +1,19 @@
 from lcs import *
 
 def main():
-    print(f"Hello World")
+    """
+    The main function that helps demonstrate the implementation of
+    the Longest Common Sequence (LCS) algorithm between two strings.
+    """
+    s1 = "mangoes"
+    s2 = "mementos"
+    
+    lcs, lcs_length = get_lcs(s1, s2)
+    lcs_str = lcs_string(s1, s2, lcs)
+    
+    # Results
+    print(f"LCS Length: {lcs_length}")
+    print(f"Longest Common Subsequence: {lcs_str}")
     
 # Run the main function
 if __name__ == "__main__":
